c693f465e3b77453ed7edd028cc36c3a23b8d398
Azul Morales <nath.mint4@gmail.com>
2026-05-01 19:06:57 -0700

    organización

---
+++ main.py
@@ -0,0 +1,19 @@
+import time 
+import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
+
+
+#Creación de la ventana principal
+root= tk.Tk()
+root.title("Sistema de un Restaurante")
+root.geometry("800x600") 
+
+root.iconbitmap('recursos\\favicon.ico')
+
+
+
+
+
+#Esto tiene que ir al final del código, por lo que si se va a agregar algo, recuerde eso
+root.mainloop()
