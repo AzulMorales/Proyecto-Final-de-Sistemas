e5bb871ba7e38a2170904ef2ee975593e366a309
Azul Morales <nath.mint4@gmail.com>
2026-05-07 19:05:12 -0700

    cambios ahora si de interfaz y codigo

---
+++ Proyecto/sistema.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+import time
+
+class sistema_operativo:
+    def __init__(self):
+        self.root = None
+        self.text_area = None
+
+        self.cola_pedidos = []
+        self.historial_pedidos = []
+
+    def set_contexto(self, root, text_area):
+        self.root = root
+        self.text_area = text_area
+
+    def agregar_pedido(self, id, comida, tiempo_preparacion):
+        pedido = {"id": id, "comida": comida, "tiempo_preparacion": tiempo_preparacion}
+
+        self.cola_pedidos.append(pedido)
+        self.historial_pedidos.append(pedido)
+        self.text_area.insert(tk.END, f"Pedido '{comida}' agregado a la cola.\n")
+        #La vista siemore baja
+        self.text_area.see(tk.END)
+
+    def procesar_pedidos(self):
+        self.text_area.insert(tk.END, "\n--- Iniciando cajero (FCFS) ---\n")
+        self.text_area.see(tk.END)
+        if not self.cola_pedidos:
+            self.text_area.insert(tk.END, "No hay pedidos pendientes en la cola.\n")
+            self.text_area.see(tk.END)
+            return
+        
+        self._procesar_siguiente_pedido()
+
+    def _procesar_siguiente_pedido(self):
+        if not self.cola_pedidos:
+            return
+
+        # FCFS: Sale el primero de la cola
+        pedido = self.cola_pedidos.pop(0) 
+        self.text_area.insert(tk.END, f"Procesando pedido: '{pedido['comida']}'\n")
+        self.text_area.see(tk.END)
+        
+        tiempo_segundos = pedido['tiempo_preparacion']
+        
+        if tiempo_segundos > 0:
+            self.text_area.insert(tk.END, f"Preparando... ({tiempo_segundos} segundos)\n")
+            self.text_area.see(tk.END)
+            #segundos a milisegundos para que sea bien rapidin
+            tiempo_ms = tiempo_segundos * 1000
+            self.root.after(tiempo_ms, lambda: self._finalizar_pedido(pedido))
+        else:
+            self._finalizar_pedido(pedido)
+
+    def _finalizar_pedido(self, pedido):
+        self.text_area.insert(tk.END, f"¡Pedido '{pedido['comida']}' listo para servir!\n")
+        self.text_area.see(tk.END)
+        
+        if self.cola_pedidos:
+            self.text_area.insert(tk.END, "Siguiente pedido en la cola...\n")
+            self.text_area.see(tk.END)
+            self._procesar_siguiente_pedido()
+        else:
+            self.text_area.insert(tk.END, "No hay más pedidos pendientes.\n")
+            self.text_area.see(tk.END) 
+            self.text_area.insert(tk.END, "Todos los pedidos han sido procesados.\n\n")
+            self.text_area.see(tk.END) 
+
+    def mostrar_historial(self):
+        if not self.historial_pedidos:
+            self.text_area.insert(tk.END, "\nEl historial está vacío.\n")
+            self.text_area.see(tk.END) 
+            return
+        
+        self.text_area.insert(tk.END, "\n--- Historial de Pedidos ---\n")
+        for pedido in self.historial_pedidos:
+            self.text_area.insert(
+                tk.END,
+                f"ID: {pedido['id']} | Comida: {pedido['comida']} | Tiempo: {pedido['tiempo_preparacion']}s\n" )
+            self.text_area.see(tk.END) 
+        self.text_area.insert(tk.END, "----------------------------\n\n")
+        self.text_area.see(tk.END) 
+
+    def limpiar_historial(self):
+        self.historial_pedidos.clear()
+        self.text_area.insert(tk.END, "\nHistorial limpiado correctamente.\n")
+        self.text_area.see(tk.END) 
+
+
--- main.py
+++ main.py
@@ -2,75 +2,255 @@ import time
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox, font
-from Proyecto.crearProcesos import sistema_operativo
+from PIL import Image, ImageTk
+from Proyecto.sistema import sistema_operativo
+
+sistemaOp = sistema_operativo()
+ventana_actual = None
+
+#Centrar la ventana padre
+def centrar_ventana(ventana,ancho,alto):
+    
+    pantalla_ancho = ventana.winfo_screenwidth()
+    pantalla_alto = ventana.winfo_screenheight()
+    x = int((pantalla_ancho/2) - (ancho/2))
+    y = int((pantalla_alto/2) - (alto/2))
+    return ventana.geometry(f"{ancho}x{alto}+{x}+{y}")
+
+#estetica, en el que cambie de color el boton al tocarlo
+def tocar_boton(e):
+    
+    e.widget['background'] = "#F57C00" 
+
+def no_tocar_boton(e):
+    e.widget['background'] ="#DDB885"
 
 #Creación de la ventana principal
-
 root= tk.Tk()
 root.title("Sistema de un Restaurante")
-root.geometry("800x600") 
+centrar_ventana(root, 800,600)
 root.iconbitmap('recursos\\favicon.ico')
-root.configure(bg="#edcc8a")
-
-frame = tk.Frame(root)
-frame.pack(pady=10)
-text_area = tk.Text(root, width=70, height=30)
-text_area.pack()
+root.configure(bg="#B22222")
 root.resizable(False, False)
 
+#texto
+bienve = tk.Label(root,text="Bienvenido a este pequeño sistema que simula los procesos de cualquier tipo de restaurante.",font=("Helvetica", 17, "bold"),
+                  background="#B22222",foreground="white", wraplength= 380,justify="right" )
 
-sistemaOp = sistema_operativo(root, text_area)
+bienve.place(relx=0.5, y=80, anchor="w")
+
+#Imagen de un chef
+frame_derecha = tk.Frame(root, bg="#B22222")
+frame_derecha.pack(side=tk.RIGHT, fill="y")
+#Redimensionar
+immagen = Image.open("recursos\\chef.png")
+imagen = immagen.resize((350, 350))   
+imagen = ImageTk.PhotoImage(imagen)
+
+# Un label como en Java
+label_imagen = tk.Label(root, image=imagen, bg="#B22222")
+label_imagen.image = imagen 
+
+label_imagen.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-10)
 
 
-# El usuario escribe el tiempo
-def pedir_tiempo(nombre_comida):
+#Menu izquierdo
+def crear_menu(ventana):
+    menu_izq = tk.Frame(ventana, width=130, bg="#8E7462")
+    menu_izq.pack(side=tk.LEFT, fill="y")
+
+    #Ocultar y mostrar el menu lateral
+    def ocultar_ver_menu():
+        #ocultar
+        if menu_izq.winfo_ismapped():
+            menu_izq.pack_forget()
+        else:
+            # Reaparece exactamente en su posición original
+            menu_izq.pack(side=tk.LEFT, fill="y")
+
+    boton_menu = tk.Button(ventana,text="≡",font="Verdana 15 bold",width=3, background="#DDB885", 
+                        borderwidth=3,command=ocultar_ver_menu,)
+    #Para que el boton no dependa de ninguno y no se mueva
+    boton_menu.place(x=10, y=10)
+
+    boton_menu.bind("<Enter>", tocar_boton)
+    boton_menu.bind("<Leave>", no_tocar_boton)       
+
+    #Botones del menu
+    boton_crear_proceso = tk.Button(menu_izq,text="Crear Proceso",font="Helvetica 14", background="#DDB885", borderwidth=2,
+                                    command= lambda: cambiar_ventana(crear_pedidos))
+    boton_crear_proceso.pack(fill="x", pady=75)
+
+    boton_crear_proceso.bind("<Enter>", tocar_boton)
+    boton_crear_proceso.bind("<Leave>", no_tocar_boton)
+                            
+    boton_ver_proceso = tk.Button(menu_izq,text="Ver Proceso",font="Helvetica 14", background="#DDB885",borderwidth=2, 
+                                  command=lambda: cambiar_ventana(ver_procesos))
+    boton_ver_proceso.pack(fill="x", pady=0)
+
+    boton_ver_proceso.bind("<Enter>", tocar_boton)
+    boton_ver_proceso.bind("<Leave>", no_tocar_boton)
+
+    boton_eliminar_proceso = tk.Button(menu_izq,text="Eliminar Proceso",font="Helvetica 14", background="#DDB885",borderwidth=2,
+                                       command=lambda: cambiar_ventana(eliminar_procesos))
+    boton_eliminar_proceso.pack(fill="x", pady=75)
+
+    boton_eliminar_proceso.bind("<Enter>", tocar_boton)
+    boton_eliminar_proceso.bind("<Leave>", no_tocar_boton)
+
+def crear_ventana(titulo):
+    global ventana_actual
+    # Cerrar la ventana anterior
+    if ventana_actual is not None:
+        ventana_actual.destroy()
+
     ventana = tk.Toplevel(root)
-    ventana.title("Tiempo de preparación")
-    ventana.geometry("300x150")
+    ventana.title(titulo)
+    centrar_ventana(ventana, 800, 600)
+    ventana.configure(bg="#B22222")
     ventana.resizable(False, False)
 
+    # Frame contenedor del contenido
+    ventana.contenido = tk.Frame(ventana, bg="#B22222")
+    ventana.contenido.pack(side="right", fill="both", expand=True)
 
-    ventana.update_idletasks()
-    ancho = 300
-    alto = 150
+    # Crear menú en esta ventana
+    crear_menu(ventana)
+    ventana_actual = ventana
+    return ventana
 
-    # Obtener posición de la ventana principal
-    x_root = root.winfo_x()
-    y_root = root.winfo_y()
-    w_root = root.winfo_width()
-    h_root = root.winfo_height()
-
-    x = x_root + (w_root // 2) - (ancho // 2)
-    y = y_root + (h_root // 2) - (alto // 2)
-
-    ventana.geometry(f"{ancho}x{alto}+{x}+{y}")
-
-    tk.Label(ventana, text=f"Tiempo para {nombre_comida} (segundos):").pack(pady=10)
-    entrada = tk.Entry(ventana)
-    entrada.pack()
-
-    #boton de confirmar
-    def confirmar():
-        tiempo = entrada.get().strip()
-        if not tiempo.isdigit():
-            messagebox.showerror("Error", "Debes ingresar un número válido.")
-            return
-        sistemaOp.agregar_pedido(nombre_comida, int(tiempo))
-        ventana.destroy()
-    ttk.Button(ventana, text="Aceptar", command=confirmar).pack(pady=10)
-
-# Botones para agregar pedidos
-ttk.Button(frame, text="Hamburguesa",
-           command=lambda: pedir_tiempo("Hamburguesa")).grid(row=0, column=0, padx=5)
-
-ttk.Button(frame, text="Tacos de birria",
-           command=lambda: pedir_tiempo("Tacos de birria")).grid(row=0, column=1, padx=5)
-
-ttk.Button(frame, text="Boneless",
-           command=lambda: pedir_tiempo("Boneless")).grid(row=0, column=2, padx=5)
+#cambiar de ventana si perder los datos
+def cambiar_ventana(funcion):
+    funcion()
 
 
+#Ventana para crear pedidos
+def crear_pedidos():
+    ventana = crear_ventana("Crear Pedido")
+    ventana.iconbitmap('recursos\\favicon.ico')
+    
+    # Frame para botones
+    frame1 = tk.Frame(ventana.contenido, bg="#B22222")
+    frame1.pack(side="top", pady=10)
+
+    # Área de texto
+    text_area = tk.Text(ventana.contenido, width=70, height=30)
+    text_area.pack(pady=10)
+
+    sistemaOp.set_contexto(ventana, text_area)
+    
+  
+
+    # El usuario escribe el tiempo
+    def pedir_tiempo(nombre_comida):
+        ventana2 = tk.Toplevel(ventana)
+        ventana2.title("Tiempo de preparación")
+        ventana2.resizable(False, False)
+    
+        # Obtener posición de la ventana principal
+        ancho = 300
+        alto = 150
+        ventana2.update_idletasks()
+        x_root = ventana.winfo_x()
+        y_root = ventana.winfo_y()
+        w_root = ventana.winfo_width()
+        h_root = ventana.winfo_height()
+        x = x_root + (w_root // 2) - (ancho // 2)
+        y = y_root + (h_root // 2) - (alto // 2)
+
+        ventana2.geometry(f"{ancho}x{alto}+{x}+{y}")
+
+        #label que sale en ventana
+        tk.Label(ventana2, text=f"Tiempo para {nombre_comida} (segundos):").pack(pady=10)
+        entrada = tk.Entry(ventana2)
+        entrada.pack()
+
+        #boton de confirmar
+        def confirmar():
+            tiempo = entrada.get().strip()
+            if not tiempo.isdigit():
+                messagebox.showerror("Error", "Debes ingresar un número válido.")
+                return
+            sistemaOp.agregar_pedido(len(sistemaOp.historial_pedidos) + 1,nombre_comida,int(tiempo))
+            ventana2.destroy()
+
+        ttk.Button(ventana2, text="Aceptar", command=confirmar).pack(pady=10)
 
 
+    #botones del crear procesos
+    boton_hamburguesa = tk.Button(frame1,text="Hamburguesa",font=("Helvetica 13"), background="#DDB885", borderwidth=2, 
+                                  command=lambda: pedir_tiempo("Hamburguesa"))
+    boton_hamburguesa.pack(padx=10, side="left")
+    boton_hamburguesa.bind("<Enter>", tocar_boton)
+    boton_hamburguesa.bind("<Leave>", no_tocar_boton)   
+
+    boton_tacos = tk.Button(frame1, text="Tacos de Birria",font=("Helvetica 13"), background="#DDB885", borderwidth=2, 
+                            command=lambda: pedir_tiempo("Tacos de Birria"))
+    boton_tacos.pack(padx=10, side="left")
+    boton_tacos.bind("<Enter>", tocar_boton)
+    boton_tacos.bind("<Leave>", no_tocar_boton)
+
+    boton_boneless = tk.Button(frame1, text="Boneless",font=("Helvetica 13"), background="#DDB885", borderwidth=2,
+                               command=lambda: pedir_tiempo("Boneless"))
+    boton_boneless.pack(padx=10, side="left")
+    boton_boneless.bind("<Enter>", tocar_boton)
+    boton_boneless.bind("<Leave>", no_tocar_boton)
+  
+
+def ver_procesos():
+    ventana= crear_ventana("Ver Procesos")
+    ventana.iconbitmap('recursos\\favicon.ico')
+
+    # Frame horizontal
+    frame_botones = tk.Frame(ventana.contenido, bg="#B22222")
+    frame_botones.pack(side="top", pady=10)
+
+    # Área de texto
+    text_area = tk.Text(ventana.contenido, width=70, height=30)
+    text_area.pack(pady=10)
+
+    sistemaOp.set_contexto(ventana, text_area)
+
+   
+    #Agregar botones
+    boton_procesar = tk.Button(frame_botones, text="Procesar pedidos", font="Helvetica 13" ,background="#DDB885", command=sistemaOp.procesar_pedidos)
+    boton_procesar.pack( side="left", padx=10)
+    boton_procesar.bind("<Enter>", tocar_boton)
+    boton_procesar.bind("<Leave>", no_tocar_boton)
+
+    boton_historial = tk.Button(frame_botones, text="Mostrar historial", font="Helvetica 13" ,background="#DDB885", command=sistemaOp.mostrar_historial)
+    boton_historial.pack(side="left",padx=10)
+    boton_historial.bind("<Enter>", tocar_boton)
+    boton_historial.bind("<Leave>", no_tocar_boton)
+
+    boton_limpiar = tk.Button(frame_botones, text="Limpiar historial", font="Helvetica 13" , background="#DDB885", command=sistemaOp.limpiar_historial)
+    boton_limpiar.pack(side="left",padx=10)
+    boton_limpiar.bind("<Enter>", tocar_boton)
+    boton_limpiar.bind("<Leave>", no_tocar_boton)
+
+
+def eliminar_procesos():
+    ventana= crear_ventana("Eliminar Procesos")
+    ventana.iconbitmap('recursos\\favicon.ico')
+
+    # Frame para botones
+    frame_botones = tk.Frame(ventana.contenido, bg="#B22222")
+    frame_botones.pack(side="top", pady=10)
+
+    # Área de texto
+    text_area = tk.Text(ventana.contenido, width=70, height=30)
+    text_area.pack(pady=10)
+
+    sistemaOp.set_contexto(ventana, text_area)
+
+    #Agregar botones
+    boton_elim_proceso = tk.Button(frame_botones,text="Eliminar Procesos",font="Helvetica 13", background="#DDB885",borderwidth=2,
+                                       command=lambda: cambiar_ventana(eliminar_procesos))
+    boton_elim_proceso.pack(side= "left", pady=10)
+
+    boton_elim_proceso.bind("<Enter>", tocar_boton)
+    boton_elim_proceso.bind("<Leave>", no_tocar_boton)
+
+crear_menu(root)
 #Esto tiene que ir al final del código, por lo que si se va a agregar algo, recuerde eso
 root.mainloop()
